abca91f505eab193fe7fdda545985695e5ab419c
pedroggbcampos <pedrocasgcampos@live.com.pt>
2018-11-01 21:05:07 +0000

    added function to make geometries

--- js/GraphicalEntity.js
+++ js/GraphicalEntity.js
@@ -6,7 +6,7 @@
 /**
  * Generic Object - basically a decorated THREE.js Object3D
  */
-class GraphicalEntity extends THREE.Geometry {
+class GraphicalEntity extends THREE.Object3D {
   constructor(){
     super()
   }
@@ -17,22 +17,17 @@ class GraphicalEntity extends THREE.Geometry {
 class Wing extends GraphicalEntity {
   constructor() {
     super()
-    var material = new THREE.MeshStandardMaterial( { color : 0x00cc00 } );
-    this.vertices.push( new THREE.Vector3( 5, 0, 0 ) );
-	this.vertices.push( new THREE.Vector3( 0, 5, 0 ) );
-	this.vertices.push( new THREE.Vector3( 0, 0, 5 ) );
+    var vertices = [];
+    var material = new THREE.MeshBasicMaterial( { color : 0x00cc00 } );
+    vertices.push( new THREE.Vector3( 5, 0, 0 ) );
+	  vertices.push( new THREE.Vector3( 0, 5, 0 ) );
+  	vertices.push( new THREE.Vector3( 0, 0, 5 ) );
+    vertices.push( new THREE.Vector3( 5, 0, 0 ) );
+    vertices.push( new THREE.Vector3( 0, 0, 5 ) );
+    vertices.push( new THREE.Vector3( 5, 0, 5 ) );
 
-	var normal = new THREE.Vector3( 1, 1, 1 ); //optional
-	var color = new THREE.Color( 0x00ff00 ); //optional
-	var materialIndex = 0; //optional
-	var face = new THREE.Face3( 0, 1, 2, normal, color, materialIndex );
-
-	this.faces.push(face);
-
-	this.computeFaceNormals();
-	this.computeVertexNormals();
-
-    scene.add(new THREE.Mesh(this, material))
+    this.Mesh = constructGeometry(vertices, material)
+    scene.add(this.Mesh)
 
   }
 
--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,30 @@
 function randFloat(min, max) {
   return Math.random() * (max - min) + min;
 }
+
+/**
+  *Function that constructs a given geometry and returns its Mesh
+      vertices, positions: array of THREE.Vector3()
+      material: for example new THREE.MeshBasicMaterial()
+      rgb: new THREE.Color() type
+  */
+function constructGeometry(vertices, material) {
+  var geometry = new THREE.Geometry();
+  console.log(vertices)
+  for (var vertice in vertices)
+    geometry.vertices.push(vertices[vertice]);
+  var normal = new THREE.Vector3();
+  for (var i = 0; i < vertices.length; i += 3){  // vertices / 3  = number of faces
+      console.log(i)
+      var face = new THREE.Face3( i, i + 1, i + 2, normal);
+      geometry.faces.push(face);
+  }
+
+  geometry.computeFaceNormals();
+  geometry.computeVertexNormals();
+
+   new THREE.MeshBasicMaterial();
+
+  return new THREE.Mesh(geometry, material)
+
+}
